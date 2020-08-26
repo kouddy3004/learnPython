6b34ff1cb051332a540084af513168370f83f033
kosubram <kosubram@KOSUBRAM-IN.us.oracle.com>
2020-08-26 15:41:56 +0530

    Cleaning master

--- DuplicateNumber.py
+++
@@ -1,6 +0,0 @@
-list = [1,2,3,2,1]
-for i in range(len(list)):
-    for j in range(i):
-        if(list[i]==list[j]):
-            print(str(list[i])+" is duplicate")
-
--- webServicesinPython/fileHandler.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class FileHandler:
-    @staticmethod
-    def setProjectPath():
-        return os.path.dirname(__file__)
