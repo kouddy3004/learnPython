2f69305ec64ff0f696b6d6c9955bdee6d2dd8bda
Koushik Subramanian <koushiksubramaniandotn@gmail.com>
2020-10-25 22:58:33 +0530

    Removing untried folders

--- RestApi/main.py
+++
@@ -1,7 +0,0 @@
-import os
-from commonScripts.jsonHandler import JsonHandler
-
-currentPath=os.path.dirname(os.path.abspath(__file__))
-projectPath = os.path.dirname(currentPath)
-
-json=JsonHandler()
